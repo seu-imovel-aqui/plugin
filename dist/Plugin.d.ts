88a16896a8f736f28bcdd16902dbc23cd63d828c
Everton dos Santos Silva <evertonsantosilva18@gmail.com>
2021-11-10 21:15:48 -0300

    Adds library types seu-imovel-aqui for plugins

--- dist/Plugin.d.ts
+++ dist/Plugin.d.ts
@@ -1,7 +1,7 @@
-import { Property } from "./Property";
+import { Ad } from '@seu-imovel-aqui/plugin-types';
 export interface Plugin {
     /**
      * This methods is responsible for executing the scraping on the site and returning all the data found
      */
-    executeScraping(): Promise<Property[]>;
+    executeScraping(): Promise<Ad[]>;
 }
--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,9 +1,9 @@
-import { Property } from "./Property";
+import { Ad } from '@seu-imovel-aqui/plugin-types';
 
 export interface Plugin {
 
     /**
      * This methods is responsible for executing the scraping on the site and returning all the data found
      */
-    executeScraping(): Promise<Property[]>
+    executeScraping(): Promise<Ad[]>
 }
