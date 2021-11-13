586105f13ae2d63413e76b19f3d00346f9abc7c8
Everton dos Santos Silva <evertonsantosilva18@gmail.com>
2021-11-13 10:52:52 -0300

    Adds eslint and editorconfig

--- dist/Plugin.d.ts
+++ dist/Plugin.d.ts
@@ -1,4 +1,4 @@
-import { Ad } from '@seu-imovel-aqui/plugin-types';
+import { Ad } from "@seu-imovel-aqui/plugin-types";
 export interface Plugin {
     /**
      * This methods is responsible for executing the scraping on the site and returning all the data found
--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,9 +1,9 @@
-import { Ad } from '@seu-imovel-aqui/plugin-types';
+import { Ad } from "@seu-imovel-aqui/plugin-types";
 
 export interface Plugin {
 
-    /**
-     * This methods is responsible for executing the scraping on the site and returning all the data found
-     */
-    executeScraping(): Promise<Ad[]>
+   /**
+    * This methods is responsible for executing the scraping on the site and returning all the data found
+    */
+   executeScraping(): Promise<Ad[]>
 }
